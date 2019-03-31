288e7c7e279653ef2eaeadaa0bbc2689b5e19641
Leon <leon@MacBook-Leon.local>
2019-04-01 00:11:36 +0300

    Lesson3

---
+++ Lesson3/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Menu from './Menu';
+
+
+class App extends React.Component {
+  render() {
+    const menuItems = [
+      {href:'/', title: 'Главная'},
+      {href:'/about', title: 'О нас'},
+      {href:'/service', title: 'Услуги'},
+      {href:'/contacts', title: 'Контакты'},
+    ];
+
+    return <div>
+      <h1>Первое приложение на React</h1>
+      <p>Hello React!</p>
+      <Menu items={menuItems} titleMenu="Основное меню сайта"/>
+    </div>
+  }
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
---
+++ Lesson3/src/Menu.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import MenuItem from './MenuItem';
+
+class Menu extends React.Component {
+  render() {
+    const items = this.props.items.map((item, index) => {
+      return <MenuItem key={index} href={item.href}>{item.title}</MenuItem>
+    })
+    
+    return (
+      <div>
+        <h1>{this.props.titleMenu}</h1>
+        <ul>
+          {items}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default Menu;
+
+// const menuItems = [
+//   {href:'/', title: 'Главная'},
+//   {href:'/about', title: 'О нас'},
+//   {href:'/service', title: 'Услуги'},
+//   {href:'/contacts', title: 'Контакты'},
+// ];
