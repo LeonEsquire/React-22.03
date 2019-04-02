c05b454369d00a6ffb7d13cbba2330d5193fda40
anton1evdokimov <36256052+anton1evdokimov@users.noreply.github.com>
2019-04-02 11:30:21 +0300

    Create Login.js

---
+++ Lesson3/src/Login.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <>
+        <form action="/login">
+        <p>
+            <strong>Логин:</strong>
+            <input type="text" name="login" maxlength="30" size="39">
+        </p>
+        <p>
+            <strong>Пароль:</strong>
+            <input type="password" name="password" maxlength="30" size="39">
+        </p>
+        <input type="submit" value="Login/Войти">
+    </form>
+      </>
+    );
+  }
+}
+
+export default Login;
