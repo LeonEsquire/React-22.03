eb62629267ab2413ac61dd0b8ba7cd4cf90a03cc
anton1evdokimov <36256052+anton1evdokimov@users.noreply.github.com>
2019-04-05 14:21:08 +0300

    Create Blog.js

---
+++ Lesson4/src/app/components/Blog.js
@@ -0,0 +1,33 @@
+    
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Post from './Post';
+
+export default class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false
+    };
+  }
+ const items = this.props.items.map(item => {
+      return <MenuItem key={item.id} body={item.postBody} title={item.title} date={item.date}/>
+    })
+  //static getDerivedStateFromProps(props, state) {
+  //  console.log('2. getDerivedStateFromProps(props, state)');
+  //  return null;
+  //}
+
+  render() {
+    return (
+      <>
+      <h1>{this.props.titleBlog}</h1>
+        {items}
+      </>
+    );
+  }
+
+  componentDidMount() {
+    console.log('4. componentDidMount()')
+  }
+}
