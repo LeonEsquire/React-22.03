073a189a68aab229a293202687d32e723e8f4f30
anton1evdokimov <36256052+anton1evdokimov@users.noreply.github.com>
2019-04-09 13:37:05 +0300

    Add pages

---
+++ Lesson5/src/app/pages/Comment.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+import OneComment from '../components/Comment';
+
+export default class Comment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      comment: null
+    };
+  }
+
+  render()
+  {
+    return(      
+      <div>
+        {this.state.comment && <OneComment {...this.state.comment}/>}
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/comments/' + this.props.params.commentId)
+    .then(response => {
+      console.log(response);
+      this.setState({comment: response.data})
+    })
+  }
+}
