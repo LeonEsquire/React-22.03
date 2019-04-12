d7dddb1701c77fa984b7bae81fd7b2a4678974ad
anton1evdokimov <36256052+anton1evdokimov@users.noreply.github.com>
2019-04-12 09:23:40 +0300

    userStore.js

---
+++ Lesson6/Lesson6_2/src/app/stores/userStore.js
@@ -0,0 +1,45 @@
+import { ADD_USER, GET_USERS } from "../constants/userConstants";
+import dispatcher from '../dispatcher';
+import {EventEmitter} from 'events';
+
+class userStore extends EventEmitter {
+  constructor(){
+    super();
+    this.users = [];
+    this.getUsers = this.getUsers.bind(this);
+    this.change = this.change.bind(this);
+    this.addUser = this.addUserind(this);
+    this.handleActions = this.handleActions.bind(this);
+  }
+
+  getUsers(users)
+  {
+    this.users = users;
+    this.change();
+  }
+
+  change() {
+    this.emit('change');
+  }
+
+  addUser(post)
+  {
+    this.users = [user, ...this.users];
+    this.change()
+  }
+
+  handleActions(action)
+  {
+    switch (action.type) {
+      case ADD_USER:UserPost(action.data)
+        break;
+      case GET_USERS:
+        this.getUsers(action.data)
+        break;
+    }
+  }
+}
+
+const store = new userStore();
+dispatcher.register(store.handleActions)
+export default store;
