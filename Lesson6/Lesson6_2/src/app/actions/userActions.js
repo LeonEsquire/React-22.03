e19a1b0037f6cd17715618230dc132e149bb593b
anton1evdokimov <36256052+anton1evdokimov@users.noreply.github.com>
2019-04-12 09:46:23 +0300

    Add phone, website

--- Lesson6/Lesson6_2/src/app/actions/userActions.js
+++ Lesson6/Lesson6_2/src/app/actions/userActions.js
@@ -2,10 +2,10 @@ import dispatcher from "../dispatcher";
 import { ADD_USER, GET_USERS } from "../constants/userConstants";
 import axios from "axios";
 
-export function addUser(name, email, username, id) {
+export function addUser(name, email, username, id, phone, website) {
   dispatcher.dispatch({
     type: ADD_USER,
-    data: { name, email, username, id }
+    data: { name, email, username, id, phone, website }
   });
 }
 
